2fc38e6ed37634017eb6f127cd5cf7fecbae33cc
zdnmz <zehra.donmez.18@gmail.com>
2023-04-19 22:27:58 +0300

    Update object.m

--- object.m
+++ object.m
@@ -7,57 +7,46 @@ function object_detection_and_recognition_GUI
         'Position', [315, 280, 70, 25], 'Callback', @loadcamerabutton_Callback);
     hstopcamera = uicontrol('Style', 'pushbutton', 'String', 'Stop Camera', ...
         'Position', [315, 230, 70, 25], 'Callback', @stopcamerabutton_Callback, 'Enable', 'off');
-    hcaptureimage = uicontrol('Style', 'pushbutton', 'String', 'Capture Image', ...
-        'Position', [315, 180, 70, 25], 'Callback', @captureimagebutton_Callback, 'Enable', 'off');
+  
     haxes = axes('Units', 'Pixels', 'Position', [50, 60, 200, 200]);
     htext = uicontrol('Style', 'text', 'String', 'Select a Camera to Detect and Recognize Objects', ...
         'Position', [50, 315, 300, 15]);
 
     % Initialize the UI
-    align([hloadcamera, hstopcamera, hcaptureimage, htext], 'Center', 'None');
+    align([hloadcamera, hstopcamera, htext], 'Center', 'None');
 
     % Make the UI visible
     f.Visible = 'on';
 
     % Load camera button callback function
     function loadcamerabutton_Callback(source, eventdata)
-        cam = webcam;
-        net = alexnet;
-        set(hloadcamera, 'Enable', 'off');
-        set(hstopcamera, 'Enable', 'on');
-        set(hcaptureimage, 'Enable', 'on');
-        while ishandle(f)
-            img = snapshot(cam);
-            imshow(img, 'Parent', haxes);
-            drawnow;
+    cam = webcam;
+    pause(3); % Wait for 3 seconds
 
-            % Perform object detection and recognition using AlexNet
-            img = imresize(img, [227 227]);
-            label = classify(net, img);
-            title(haxes, char(label));
-        end
-        clear cam;
+    net = alexnet;
+    set(hloadcamera, 'Enable', 'off');
+    set(hstopcamera, 'Enable', 'on');
 
-        htext.String = 'Camera Stopped';
-        set(hloadcamera, 'Enable', 'on');
-        set(hstopcamera, 'Enable', 'off');
-        set(hcaptureimage, 'Enable', 'off');
-    end
+    img = snapshot(cam);
+    
+    imshow(img, 'Parent', haxes);
+    drawnow;
+
+    % Perform object detection and recognition using AlexNet
+    img = imresize(img, [227 227]);
+    label = classify(net, img);
+    title(haxes, char(label));
+
+    clear cam;
+
+    htext.String = 'Camera Stopped';
+    set(hloadcamera, 'Enable', 'on');
+    set(hstopcamera, 'Enable', 'on');
+end
 
     % Stop camera button callback function
     function stopcamerabutton_Callback(source, eventdata)
         close(f);
     end
 
-    % Capture image button callback function
-    function captureimagebutton_Callback(source, eventdata)
-        % Get the current snapshot from the camera
-        img = snapshot(cam);
-
-        % Save the image as a JPEG file
-        imwrite(img, sprintf('image_%s.jpg', datestr(now, 'yyyymmdd_HHMMSS')));
-
-        % Update the text label
-        htext.String = 'Image Captured and Saved';
-    end
 end
